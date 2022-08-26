e814e5dcf7f6d2f102b1cb3f945439fae24d2faf
Brendan Barnes <barne856@umn.edu>
2022-08-26 16:34:59 -0500

    .gitignore update

--- build/tests_include-b12d07c.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/bb/DEV/LIT/build/tests_tests-b12d07c.cmake")
-  include("/home/bb/DEV/LIT/build/tests_tests-b12d07c.cmake")
-else()
-  add_test(tests_NOT_BUILT-b12d07c tests_NOT_BUILT-b12d07c)
-endif()
